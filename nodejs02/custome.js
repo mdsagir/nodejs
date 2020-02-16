22cbb4d33993f966a19e2863478a96501d5bdcc3
mdsagir <tech.sagir@gmail.com>
2020-02-16 11:38:02 +0530

    deleting some old code

--- nodejs02/custome.js
+++
@@ -1,7 +0,0 @@
-module.exports={
-    dt:function() {
-        return Date();
-    },
-    mystr1: "first String ",
-    mystr2:"second string"
-};
--- nodejs02/evaluate.js
+++
@@ -1,8 +0,0 @@
-function sum(a,b){
-    return a+b;
-}
-
-var str="This is string";
-
-exports.sum=sum;
-exports.str=str;
